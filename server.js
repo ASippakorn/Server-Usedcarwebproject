73659e0525f6256098ccc22f977ac5273d9a76fc
ASippakorn <able.sipp@outlook.com>
2024-11-25 13:56:23 +0700

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const cors = require('cors')
 //middleware
 const app = express();
 app.use(cors({//อยู่ก่อนrouter
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","localhost:5173"],
     credentials: true,
 }))
 app.use(express.json());
